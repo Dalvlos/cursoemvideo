b85d8aa38dacd7cbfef46bb5fc452a895183d806
Dalvlos <dalvlos@yahoo.co.jp>
2023-12-03 21:42:36 +0900

    Resolution of exercice 84.

--- ex084.py
+++ ex084.py
@@ -31,3 +31,39 @@ print(f'As pessoas mais pesadas em ordem {lista_ordenada2}.')
 print(f'As pessoas mais leves em ordem {lista_ordenada}.')
 
 
+"""
+resposta curso em video
+
+temp = []
+princ = []
+mai = men = 0
+while True:
+    temp.append(str(input('Name: ')))
+    temp.append(float(input('Wheight: ')))
+    if len(princ) == 0
+        mai = men temp[1]
+    else:
+        if temp[1] > mai:
+            mai = temp[1]
+        if temp[1] < men:
+            men = temp[1]
+    princ.append(temp[:])
+    temp.clear()
+    resp = str(input('Continue? [S/N] '))
+    if resp in 'Nn':
+        break
+
+print('=-*30)
+print(f'In total you registered {len(princ)} people.')
+print(f'The most havier was {mai}Kg.', end='')
+for p in princ:
+    if p[1] == mai:
+        print(f'[{p[0]}] ', end='')
+print()
+print(f'the lowest weight was {men}Kg. ', end='')
+for p in princ:
+    if p[1] == men:
+        print(f'[{p[0]}] ', end='')
+print()
+    
+"""
