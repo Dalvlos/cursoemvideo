9a2929ec5c0b1c4c6c353cdff3f8182c7ed37ef7
Dalvlos <dalvlos@yahoo.co.jp>
2023-12-09 22:28:20 +0900

    Resolution of exercise 85

--- ex085.py
+++ ex085.py
@@ -6,3 +6,14 @@ que mantenha separados os valores pares e impares.
 No final, mostre os valores pares e impares em ordem crescente
 
 """
+
+numeros = []
+for _ < 8:
+    numero = int(input('Enter number: '))
+    
+    if numero % 2 == 0:
+        numeros.insert(0, numeros)
+    else:
+        numeros.append(numeros)  
+
+print("Lista final:", numeros)
